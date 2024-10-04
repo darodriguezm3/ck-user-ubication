7917cb70638d26f2cdb5ebb8cc3d3b2d35f347c4
daniro372 <daniro372@gmail.com>
2024-10-04 22:15:54 +0000

    add model and set project schema

---
+++ Models/Ubication.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Identity;
+
+public class Town
+{
+    public int TownId { get; set; }
+    public string Name { get; set; }
+    public IdentityError DepartmentId { get; set; }
+}
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:813787587.
+public class Department
+{
+    public int DepartmentId { get; set; }
+    public string Name { get; set; }
+    public int CountryId { get; set; }
+}
+
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:504023904.
+public class Country
+{
+    public int CountryId { get; set; }
+    public string Name { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+public class User
+{
+    public int UserId { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public int TownId { get; set; }
+}
